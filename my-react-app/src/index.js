b7e7ed3d87e4ab257697a9eb24f09379491e77ef
Juno-ko <juno-koo@pm.me>
2023-03-07 11:52:58 +0200

    Translated phrases to finnish.

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -88,8 +88,8 @@ class Game extends React.Component {
     
         const moves = history.map((step, move) => {
             const desc = move ?
-                'Go to move #' + move :
-                'Go to game start';
+                'Mene siirtoon #' + move :
+                'Mene pelin alkuun';
             return (
                 <li key={move}>
                     <button onClick={() => this.jumpTo(move)}>{desc}</button>
@@ -99,9 +99,9 @@ class Game extends React.Component {
 
         let status;
         if (winner) {
-            status = "Winner: " + winner;
+            status = "Voittaja: " + winner;
         } else {
-            status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+            status = "Seuraava siirto: " + (this.state.xIsNext ? "X" : "O");
         }
 
         return (
